6cd5962c504e2117354c5f9baa8539af4f304554
Ellen Langelaar <ellen.langelaar@nedap.com>
2021-06-08 16:07:59 +0200

    Added release and tag action

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# release-tag-action
+# Release Tag After Merge Action
+
 This Github Action tags and releases after a pull request is merged.
+
+```
+"master"-------------M----------
+                    /
+"release/1.0.0"----/
+```
+
+## Inputs
+
+### `token`
+
+**REQUIRED** A Github token, usually ${{ github.token }}.
+
+### `release-branch-prefix`
+
+The prefix of the release branch. Default `release/`. (example branch name: release/1.0.0)
+
+### `show-changelog`
+
+To show the title and body of the pull request as the changelog (body) of the release. Default `true`.
+
+## Outputs
+
+### `tag`
+
+The tag name
+
+## Example usage
+
+```yaml
+name: Release tag
+
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/release-tag-action@1.0.0
+```
---
+++ index.js
@@ -0,0 +1,96 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+run();
+
+async function run() {
+  try {
+    await action();
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+async function action() {
+  const githubToken = core.getInput("token");
+  const octokit = github.getOctokit(githubToken);
+  const tag = getTag();
+
+  await createTag(tag, octokit);
+  await createRelease(tag, octokit);
+
+  core.setOutput("tag", tag);
+}
+
+function getTag() {
+  const releaseBranchPrefix = core.getInput("release-branch-prefix");
+  const releaseBranchName = getReleaseBranchName();
+  if (
+    releaseBranchPrefix.length > 0 &&
+    !releaseBranchName.includes(releaseBranchPrefix)
+  )
+    throw new Error(
+      `Branch name doesn't include prefix. releaseBranchName={${releaseBranchName}};releaseBranchPrefix={${releaseBranchPrefix}}`
+    );
+
+  return releaseBranchName.replace(releaseBranchPrefix, "");
+}
+
+function getReleaseBranchName() {
+  const commitMessage = github.context.payload.head_commit.message;
+  if (!commitMessage.includes("Merge pull request"))
+    throw new Error(`Commit is not a merge. message={${commitMessage}}`);
+
+  const repoName = github.context.payload.repository.full_name;
+  const organization = repoName.split("/")[0];
+
+  const releaseBranchName = commitMessage
+    .split(organization)[1]
+    .split("\n\n")[0]
+    .substring(1);
+
+  return releaseBranchName;
+}
+
+async function createTag(newTag, octokit) {
+  if (!process.env.GITHUB_SHA) throw new Error(`GITHUB_SHA is missing`);
+
+  console.log("Creating tag");
+  await octokit.rest.git.createTag({
+    ...github.context.repo,
+    tag: newTag,
+    message: newTag,
+    object: process.env.GITHUB_SHA,
+    type: "commit",
+  });
+
+  console.log("Pushing new tag");
+  await octokit.rest.git.createRef({
+    ...github.context.repo,
+    ref: `refs/tags/${newTag}`,
+    sha: process.env.GITHUB_SHA,
+  });
+}
+
+async function createRelease(newTag, octokit) {
+  const showChangelog = core.getBooleanInput("show-changelog");
+  let body = " ";
+
+  if (showChangelog) {
+    const listOfPrs =
+      await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+        ...github.context.repo,
+        commit_sha: github.context.payload.head_commit.id,
+      });
+    const firstPr = listOfPrs.data[0];
+    body = `# ${firstPr.title}\n\n${firstPr.body}`;
+  }
+
+  console.log("Creating release");
+  await octokit.rest.repos.createRelease({
+    ...github.context.repo,
+    tag_name: newTag,
+    name: newTag,
+    body: body,
+  });
+}
